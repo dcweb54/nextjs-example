2585b425075175e4d607f5864184b878cc34b15a
Your Name <dc547373@gmail.com>
2020-09-09 23:46:34 -0700

    this is second commit

--- pages/with_router.tsx
+++ pages/with_router.tsx
@@ -5,6 +5,9 @@ const WithRouter = withRouter(({router:{query:{id}}})=>(
         <h1>
             welcome to withRouter api {id}
         </h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur eveniet sequi recusandae facilis, a consequuntur voluptatum provident ad repellat, illum soluta autem tempora, eligendi laboriosam. Aperiam atque nihil possimus cum?
+        </p>
     </div>
 ))
 
